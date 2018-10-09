99acbca19502d95cc0f8a7d090747d72d7a6cb7b
Pranal <pranalmadria@gmail.com>
2018-10-09 17:33:39 -0500

    Added CollectorPivot motor, changed name of a telelop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Collector/Collector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Collector/Collector.java
@@ -13,18 +13,40 @@ public class Collector extends RobotComponent {
 
 
     private DcMotor collector;
+    private DcMotor collectorPivot;
 
     public void init(RobotBase BASE)
     {
         super.init(BASE);
         collector = mapper.mapMotor("collector", DcMotorSimple.Direction.FORWARD);
         collector.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        collectorPivot = mapper.mapMotor("pivot", DcMotorSimple.Direction.REVERSE);
+        collectorPivot.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
 
-    public void run(double SPEED)
+    //Run the actual collector motor
+    public void runCollector(double SPEED)
     {
         collector.setPower(SPEED);
+    }
+
+    //Move actual collector pivot. If true is passed in, move collector down. If false
+    //is passed in, move collector up
+    public void putCollectorDown(boolean DOWN)
+    {
+        if(DOWN)
+        {
+            collectorPivot.setPower(.20);
+        }
+    }
+
+    public void putCollectorUp(boolean UP)
+    {
+        if(UP)
+        {
+            collectorPivot.setPower(-.20);
+        }
     }
 
     @Override
@@ -32,5 +54,6 @@ public class Collector extends RobotComponent {
 
     {
         collector.setPower(0);
+        collectorPivot.setPower(0);
     }
 }
