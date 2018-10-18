6016703f0db53e03eb5da0115804e11e735e04ea
Pranal <pranalmadria@gmail.com>
2018-10-18 18:03:01 -0500

    Color Sens and Utilities related to Color

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/HookLift/HookLift.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.Components.HookLift;
+
+import org.firstinspires.ftc.robotcontroller.internal.Core.RobotComponent;
+
+/**
+ * Created by pmkf2 on 10/17/2018.
+ */
+
+public class HookLift extends RobotComponent
+{
+    @Override
+    public void stop()
+    {
+
+    }
+}
