acef18d871449001af1c461570b4eed7bc2d4165
Pranal <pranalmadria@gmail.com>
2018-11-11 13:29:29 -0600

    Marker Delivery, small fix as well

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/MarkerDelivery/MarkerDelivery.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Components.MarkerDelivery;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcontroller.internal.Core.RobotComponent;
+
+/**
+ * Created by pmkf2 on 11/4/2018.
+ */
+
+public class MarkerDelivery extends RobotComponent
+{
+    public Servo markerDelivery;
+
+
+    public final double CLOSED = 0;
+    public final double OPEN = .75;
+
+    //@Override
+    public void init()
+    {
+
+    }
+    @Override
+    public void stop()
+    {
+
+    }
+}
