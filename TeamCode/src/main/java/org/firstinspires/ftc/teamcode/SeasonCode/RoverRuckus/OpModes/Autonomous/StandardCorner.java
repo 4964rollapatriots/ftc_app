1ae46da56dcf2fb90dd6c718abe80791e54f64c1
Pranal <pranalmadria@gmail.com>
2018-11-22 18:59:55 -0600

    We need to test auto! Standard Corner stuff editted
    Happy Thanksgiving :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeasonCode/RoverRuckus/OpModes/Autonomous/StandardCorner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeasonCode/RoverRuckus/OpModes/Autonomous/StandardCorner.java
@@ -7,9 +7,9 @@ import org.firstinspires.ftc.robotcontroller.internal.Core.Sensors.UtilCV;
 import org.firstinspires.ftc.teamcode.Components.MarkerDelivery.MarkerDelivery;
 import org.firstinspires.ftc.teamcode.SeasonCode.RoverRuckus.Base;
 
-@Autonomous(name = "CORNER")
+@Autonomous(name = "STANDARD CORNER")
 
-public class Corner extends LinearOpMode {
+public class StandardCorner extends LinearOpMode {
     //
     public Base _base = new Base();
     private UtilCV eye;
@@ -32,7 +32,6 @@ public class Corner extends LinearOpMode {
         eye = new UtilCV(hardwareMap);
         waitForStart();
 
-        //telemetry.addData("Test", ten);
         sleep(500);
         if (eye.isAligned()){
             _block = blockState.MIDDLE;
@@ -61,60 +60,37 @@ public class Corner extends LinearOpMode {
         telemetry.addData("block state is ", _block);
         sleep(500);
 
-        driveToDistance(20,0.15);
+        driveToDistance(15,0.15);
 
-//        driveToDistance(30,0.5);
-//
+        //driveToDistance(-15, .15);
+
+
 //        turnToAngle(SECOND_TURN, 0.5);
 //
-//        driveToDistance(-37, -0.7);
+//        driveToDistance(37, 0.7);
 //
 //        turnToAngle(360 - 45, 0.3);
 //
-//        driveToDistance(-48, -0.6);
+//        driveToDistance(48, 0.6);
+
 
         //dump relic
 
-//        turnToAngle(135, 0.4);
-
-        //driveToDistance( -30, -0.9);
-//        telemetry.addData("Check distance set Back Left,",  _base.drivetrain.getTargetEncoderCounts()[0]);
-//        telemetry.addData("Check distance set Back Right,", _base.drivetrain.getTargetEncoderCounts()[1]);
-//        telemetry.addData("Check distance set Front Left,", _base.drivetrain.getTargetEncoderCounts()[2]);
-//        telemetry.addData("Check distance set Front Right,", _base.drivetrain.getTargetEncoderCounts()[3]);
-//        telemetry.addData("Check current set Back Left,",  _base.drivetrain.getEncoderCounts()[0]);
-//        telemetry.addData("Check current set Back Right,", _base.drivetrain.getEncoderCounts()[1]);
-//        telemetry.addData("Check current set Front Left,", _base.drivetrain.getEncoderCounts()[2]);
-//        telemetry.addData("Check current set Front Right,", _base.drivetrain.getEncoderCounts()[3]);
-//        telemetry.update();
-
-//        _base.drivetrain.driveTo.goTo(25, .65);
-//        _base.drivetrain.driveTo.runSequentially();
-
-        // _base.drivetrain.turnTo.goTo(45, .52);
-        //_base.drivetrain.turnTo.runSequentially();
-        telemetry.addData("Angle Z: ", _base.imu.zAngle());
-        telemetry.addData("Angle X: ", _base.imu.xAngle());
-        telemetry.addData("Angle Y: ", _base.imu.yAngle());
-        telemetry.addData("SPEED: ", _base.drivetrain.frontLeft().getPower());
-        telemetry.update();
-//        switch (_block)
-//        {
-//            case LEFT:
-//                break;
-//            case RIGHT:
-//                break;
-//            case MIDDLE:
-//                break;
-//            case UNCERTAIN:
-//                break;
-//        }
-//        _base.drivetrain.turnTo.goTo(180, .70);
-//        _base.drivetrain.turnTo.runSequentially();
-
-        //_base.drivetrain.driveTo.goTo(500, .3);
-        //_base.drivetrain.driveTo.runSequentially();
+//        turnToAngle(SOME ANGLE, .50);
+//        _base.deliver.deliverMarker();
 //
+//        //re align with other crater
+//        turnToAngle(360-45, .50);
+//
+//        driveToDistance(-100, .50);
+        //DONE
+
+        _base.outTelemetry.addData("Angle Z: ", _base.imu.zAngle());
+        _base.outTelemetry.addData("Angle X: ", _base.imu.xAngle());
+        _base.outTelemetry.addData("Angle Y: ", _base.imu.yAngle());
+        _base.outTelemetry.addData("SPEED: ", _base.drivetrain.frontLeft().getPower());
+        _base.outTelemetry.update();
+
         _base.drivetrain.stop();
     }
 
@@ -125,7 +101,8 @@ public class Corner extends LinearOpMode {
     }
 
     private void driveToDistance(double inches, double speed){
-        _base.drivetrain.driveTo.goTo(inches, speed);
+        //this negative sign is to correct the negation
+        _base.drivetrain.driveTo.goTo(-inches, speed);
         _base.drivetrain.driveTo.runSequentially();
     }
 
