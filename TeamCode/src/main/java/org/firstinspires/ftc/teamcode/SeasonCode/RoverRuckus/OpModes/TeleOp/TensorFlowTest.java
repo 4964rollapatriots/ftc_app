4a329924960a1d98eb3420fc401193779c2d6a94
Unknown <joelschott3@gmail.com>
2018-12-07 23:07:12 -0600

    tensor flow test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeasonCode/RoverRuckus/OpModes/TeleOp/TensorFlowTest.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.SeasonCode.RoverRuckus.OpModes.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcontroller.internal.Core.Utility.CustomTensorFlow;
+import org.firstinspires.ftc.robotcontroller.internal.Core.Utility.UtilGoldDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.teamcode.SeasonCode.RoverRuckus.Base;
+import org.firstinspires.ftc.teamcode.SeasonCode.RoverRuckus.OpModes.Autonomous.JoelDoubleCrater;
+
+import static org.firstinspires.ftc.robotcore.external.tfod.TfodRoverRuckus.LABEL_GOLD_MINERAL;
+
+@TeleOp (name = "Tensor Flow Test")
+public class TensorFlowTest extends LinearOpMode {
+
+    private UtilGoldDetector eye;
+    private CustomTensorFlow detector;
+
+    private boolean RUN_USING_TENSOR_FLOW = false;
+    private double ACCEPTABLE_CONFIDENCE = 0.4;
+    private boolean scaleModeControl= false;
+
+    public void runOpMode(){
+
+        eye = new UtilGoldDetector(hardwareMap);
+        detector = new CustomTensorFlow(hardwareMap);
+
+        RUN_USING_TENSOR_FLOW = true;
+
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            if(gamepad1.a) {
+                if (scaleModeControl) {
+                    scaleModeControl = false;
+                    RUN_USING_TENSOR_FLOW = !RUN_USING_TENSOR_FLOW ;
+                }
+            }
+            else
+                scaleModeControl = true;
+
+            if (aligned()){
+                telemetry.addData("found block ", " now");
+            }
+        }
+
+
+    }
+
+    private boolean aligned(){
+        boolean aligned = false;
+        if (RUN_USING_TENSOR_FLOW){
+            detector.refresh();
+            if(detector.updatedRecognitions == null)
+            {
+                aligned = false;
+            }
+            else{
+                for (int i = 0; i < detector.updatedRecognitions.size(); i ++){
+                    Recognition rec = detector.updatedRecognitions.get(i);
+                    if (rec.getLabel().equals(LABEL_GOLD_MINERAL) && rec.getConfidence() > ACCEPTABLE_CONFIDENCE){
+                        aligned = true;
+                        break;
+                    }
+                }
+            }
+
+        }
+        else{
+            if (eye.isAligned()){
+                aligned = true;
+            }
+        }
+        return aligned;
+
+
+    }
+}
